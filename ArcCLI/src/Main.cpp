5b83a154e721101eace54f79d037b9da964c089f
MaikoDev <phyonaung@ymail.com>
2024-06-24 20:53:18 -0700

    Command testing

--- ArcCLI/src/Main.cpp
+++ ArcCLI/src/Main.cpp
@@ -4,14 +4,35 @@
 #include <iostream>
 #include <sstream>
 #include <iomanip>
+#include <queue>
+
+#include <commands/CommandData.h>
+#include <commands/CommandType.h>
+#include <commands/CommandFactory.h>
 
 #include <openssl/sha.h>
 
+using namespace MaikoDev::Arc;
+
 int main(int argc, char** args) {
+
+    std::unique_ptr<Commands::CommandData> commandData = Commands::extractCommand(argc, args);
+    if (!commandData) return 0;
+
+    Commands::CommandFactory factory;
+    {
+        auto t = factory.getCommand(std::move(commandData));
+    }
 
     unsigned char hash[SHA256_DIGEST_LENGTH];
     std::string inputMessage = "Hello World";
     std::stringstream ss;
+
+    std::unique_ptr<unsigned char*> testPtr = std::make_unique<unsigned char*>(new unsigned char[inputMessage.length() + 1]);
+    memcpy(*testPtr, inputMessage.c_str(), inputMessage.length());
+    (*testPtr.get())[inputMessage.length()] = (unsigned char)'\0';
+
+    std::shared_ptr<unsigned char*> shared = std::move(testPtr);
 
     SHA256((unsigned char*)inputMessage.c_str(), inputMessage.length(), hash);
 
