9c3e58c00b384d8085a6d3a8a406632dd9446300
MaikoDev <phyonaung@ymail.com>
2024-06-29 21:40:24 -0700

    Started moving code into Main that would be close to actual deployment code

--- ArcCLI/src/Main.cpp
+++ ArcCLI/src/Main.cpp
@@ -5,44 +5,45 @@
 #include <sstream>
 #include <iomanip>
 #include <queue>
+#include <filesystem>>
 
 #include <commands/CommandData.h>
 #include <commands/CommandType.h>
 #include <commands/CommandFactory.h>
 
 #include <collections/CommandKeyDictionary.h>
+#include <exceptions/UninitializedRepoException.h>
+
+#include <ArcCLI.h>
 
 #include <openssl/sha.h>
 
 using namespace MaikoDev::Arc;
 
 int main(int argc, char** args) {
+    std::string input;
 
-    std::unique_ptr<Commands::CommandData> commandData = Commands::extractCommand(argc, args);
-    if (!commandData) return 0;
+    fs::path workingDirectory = fs::current_path();
 
-    Commands::CommandFactory factory;
-    {
-        auto t = factory.getCommand(std::move(commandData));
+    fs::path arc = fs::proximate(".arc");
+
+    std::cout << fs::absolute(arc) << std::endl;
+    
+    try {
+        ArcCLI app(workingDirectory, argc, args);
+    }
+    catch (Exceptions::NonValidCommandException& ex) {
+        printf("arc: '%s' is not a arc command. See 'arc --help'.\n", ex.what());
+    }
+    catch (Exceptions::UninitializedRepoException& ex) {
+        printf("%s\n", ex.what());
     }
 
-    unsigned char hash[SHA256_DIGEST_LENGTH];
-    std::string inputMessage = "Hello World";
-    std::stringstream ss;
+    /*for (auto const& dir : fs::recursive_directory_iterator(fs::current_path())) {
+        std::cout << dir << std::endl;
+    }*/
 
-    std::unique_ptr<unsigned char*> testPtr = std::make_unique<unsigned char*>(new unsigned char[inputMessage.length() + 1]);
-    memcpy(*testPtr, inputMessage.c_str(), inputMessage.length());
-    (*testPtr.get())[inputMessage.length()] = (unsigned char)'\0';
-
-    std::shared_ptr<unsigned char*> shared = std::move(testPtr);
-
-    SHA256((unsigned char*)inputMessage.c_str(), inputMessage.length(), hash);
-
-    for (unsigned int i = 0; i < SHA256_DIGEST_LENGTH; i++)
-        std::cout << std::setfill('0') << std::setw(2) << std::hex << (int)hash[i];
-
-    std::cout << ss.str() << std::endl;
-    std::cin >> inputMessage;
+    std::cin >> input;
 
     return 0;
 }
