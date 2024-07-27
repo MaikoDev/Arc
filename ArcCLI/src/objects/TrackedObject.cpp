50cc1ff57701bf64e0dca7813f486bc797cb3e30
MaikoDev <phyonaung@ymail.com>
2024-07-26 20:31:22 -0700

    Added method to calculate checksum

--- ArcCLI/src/objects/TrackedObject.cpp
+++ ArcCLI/src/objects/TrackedObject.cpp
@@ -1,5 +1,21 @@
 #include "TrackedObject.h"
 
+#include <iomanip>
+#include <memory>
+
 using namespace MaikoDev::Arc::Objects;
 
-const std::string const TrackedObject::serialize() { return getContent(); }
+std::string const TrackedObject::serialize() { return getContent(); }
+
+std::unique_ptr<std::string> TrackedObject::checksumCalculate(const std::string& input) {
+    SHA256((unsigned char*)input.c_str(), input.length(), _checksumHash);
+
+    for (unsigned int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+        _stringBuilder << std::setfill('0') << std::setw(2) << std::hex << (int)_checksumHash[i];
+    }
+
+    std::unique_ptr<std::string> checksum = std::make_unique<std::string>(_stringBuilder.str());
+    _stringBuilder.clear();
+
+    return checksum;
+}
--- ArcCLI/src/objects/TrackedObject.h
+++ ArcCLI/src/objects/TrackedObject.h
@@ -1,8 +1,11 @@
 #pragma once
 
-#include <objects/TrackedType.h>
+#include <sstream>
 
-#include <io/ISerializable.h>
+#include "openssl/sha.h"
+
+#include "io/ISerializable.h"
+#include "objects/TrackedType.h"
 
 namespace MaikoDev {
     namespace Arc {
@@ -15,6 +18,11 @@ namespace MaikoDev {
                 virtual const std::string const& getContent() = 0;
                 virtual const TrackedType& const getType() = 0;
                 virtual const std::string& const getTypeStr() = 0;
+
+                std::unique_ptr<std::string> checksumCalculate(const std::string& input);
+            private:
+                unsigned char _checksumHash[SHA256_DIGEST_LENGTH];
+                std::stringstream _stringBuilder;
             };
         }
     }
